5e35b8caad308c80e3fd7c873e5600c0c72a1aad
Mumtazben Shaikh <mumtazben.shaikh@dreamlines.com>
2023-01-08 15:41:38 +0100

    Added linear search algorithm.

---
+++ ArrayAlgorithms/LinearSearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAlgorithms
+{
+    public static class LinearSearch
+    {
+        static Boolean LinearSearchitem(int[] input, int n)
+        {
+            foreach (int current in input)
+            {
+                if (n == current)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void ExecuteAlgorithm()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+            // Console.WriteLine(LinearSearchitem(arr, 4));
+            // Console.WriteLine(LinearSearchitem(arr, 8));
+
+            int item = Array.Find(arr, element => element == 3);
+            Console.WriteLine(item);
+            int[] items = Array.FindAll(arr, element => element >= 5);
+            Array.ForEach(items, Console.WriteLine);
+        }
+
+    }
+}
